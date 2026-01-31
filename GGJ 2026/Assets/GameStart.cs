588f2defd18a7d493422465c6d785b7dcd71cd39
Thomas Adams <tgadams764@gmail.com>
2026-01-30 22:37:55 -0500

    Added new Unity project with "Hello, World"

---
+++ GGJ 2026/Assets/GameStart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetRandomColor()
+    {
+        GetComponent<UnityEngine.UI.Image>().color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+}
