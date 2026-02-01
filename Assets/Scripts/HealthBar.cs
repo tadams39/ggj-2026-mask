75bf3897d3616763c91c8eeb781b117b4a0aa19b
bobbledev <bob.kirchner.dev@gmail.com>
2026-01-31 22:34:39 -0500

    health bar prefab

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    [SerializeField] private Image healthFill;
+
+    private float maxHealth = 100f;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthFill.fillAmount = currentHealth / maxHealth;
+    }
+}
+
